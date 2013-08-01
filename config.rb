8a162b2f6c7f80f5edadf8de7e5de45df6f711e9
Meghan Palagyi <meghanp@stanford.edu>
2013-07-31 21:14:19 -0700

    Bare bones theme files.

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require gems
+require 'sass'
+require 'compass'
+require 'breakpoint'
+require 'singularitygs'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# Directories
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+images_dir = "images"
+javascripts_dir = "js"
+
+# Output style:
+# output_style = :expanded or :nested or :compact or :compressed
+
+# Relative paths to assets via compass helper functions.
+relative_assets = true
+
+# Preferred sass syntax
+preferred_syntax = :sass
+
+# Add asset cache buster
+asset_cache_buster :none
